cb9ed57ebbc3b224941bd67c6fadddf31a65c1a6
Lea <eliasenglen2@gmail.com>
2023-05-02 13:46:20 +0200

    add new

---
+++ components/navbarComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navigation-bar">
+    <div class="content-container d-flex flex-row justify-content-between align-flex-end">
+      <pfaff-logo />
+      <div class="link-container" :class="burgerActive ? 'burger-active' : ''">
+        <nuxt-link to="/">Home</nuxt-link>
+        <nuxt-link to="/about">About</nuxt-link>
+        <nuxt-link to="/contact">Contact</nuxt-link>
+      </div>
+      <div class="burger" :class="burgerActive ? 'burger-active' : ''" @click="burgerActive = !burgerActive">
+        <div class="burger-line"></div>
+        <div class="burger-line"></div>
+        <div class="burger-line"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import pfaffLogo from './pfaffLogo.vue'
+export default {
+  components: { pfaffLogo },
+    data() {
+        return {
+            burgerActive: false
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <navbar-component />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import navbarComponent from '~/components/navbarComponent.vue'
+export default {
+  components: { navbarComponent },
+
+}
+</script>
+
+<style>
+
+</style>
