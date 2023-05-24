a8d67f73d199ce14f136d356f84d0fd86b8361e6
WhoopsImTop <eliasenglen2@gmail.com>
2023-05-24 16:36:44 +0200

    add new

--- components/footerComponent.vue
+++ components/footerComponent.vue
@@ -47,6 +47,18 @@
       </div>
       <div class="copyright">© 2023 PFAFF GMBH | Designed with ❤️ by khri8</div>
     </div>
+
+    <div class="pfaff-logo">
+      <div class="dark-circle">
+        <div class="dark-stick"></div>
+      </div>
+      <div class="gray-circle">
+        <div class="gray-stick"></div>
+      </div>
+      <div class="light-circle">
+        <div class="light-stick"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -60,5 +72,92 @@ export default {
 <style>
 .footer .content-container {
   margin: 0 auto;
+}
+
+.pfaff-logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.dark-circle {
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: var(--primary-color);
+}
+
+.dark-stick {
+  width: 8px;
+  height: 30px;
+  background: var(--primary-color);
+  position: absolute;
+  top: -70%;
+  left: 50%;
+  transform: translateX(-53%)
+}
+
+.gray-circle {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: var(--darker-gray-color);
+  animation: rotate45 3s ease-in-out infinite;
+}
+
+.gray-stick {
+  width: 7px;
+  height: 30px;
+  background: var(--darker-gray-color);
+  position: absolute;
+  top: -130%;
+  left: 50%;
+  transform: translateX(-53%)
+}
+
+.light-circle {
+  position: absolute;
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  background: #588dad;
+  animation: rotate90 3s ease-in-out infinite;
+}
+
+.light-stick {
+  width: 7px;
+  height: 30px;
+  background: #588dad;
+  position: absolute;
+  top: -220%;
+  left: 50%;
+  transform: translateX(-53%)
+}
+
+@keyframes rotate45 {
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(45deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+
+@keyframes rotate90 {
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(90deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
 }
 </style>
