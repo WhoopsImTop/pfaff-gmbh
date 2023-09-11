8f3b776a220326f73b356221a109a7006820ff6f
WhoopsImTop <eliasenglen2@gmail.com>
2023-09-11 15:51:08 +0200

    add new

--- components/rowComponent.vue
+++ components/rowComponent.vue
@@ -1,20 +1,47 @@
 <template>
-  <div :class="inComponent ? 'content-margin' : 'content-container'" class="row">
-    <div v-for="(block, index) in component.component" :key="index" :class="component.component.length > 1 ? 'col-lg-6' : 'col p-0'">
+  <div
+    :class="inComponent ? 'content-margin' : 'content-container'"
+    class="row"
+  >
+    <div
+      v-for="(block, index) in component.component"
+      :key="index"
+      :class="component.component.length > 1 ? 'col-lg-6' : 'col p-0'"
+    >
       <div v-if="block.type === 'text'">
-        <span v-if="block.smallHeadline" class="smallHeadline" style="min-height: 30px;">{{ block.smallHeadline }}</span>
+        <span
+          v-if="block.smallHeadline"
+          class="smallHeadline"
+          style="min-height: 30px"
+          >{{ block.smallHeadline }}</span
+        >
         <h2 style="min-height: 45px">{{ block.headline }}</h2>
         <p v-html="$md.render(block.text ?? '')"></p>
-        <a class="button" v-if="block.button" :href="block.button.buttonDownloadLink || block.button.buttonLink" :download="block.button.buttonDownloadLink ? true : false">{{ block.button.buttonText }}</a>
+        <a
+          class="button"
+          v-if="block.button"
+          :href="block.button.buttonDownloadLink || block.button.buttonLink"
+          :download="block.button.buttonDownloadLink ? true : false"
+          >{{ block.button.buttonText }}</a
+        >
       </div>
       <div class="content-image-container" v-if="block.type === 'image'">
-        <img class="content-image" :src="block.image" :alt="block.alt" :title="block.title" loading="lazy" />
+        <img
+          class="content-image"
+          :src="block.image"
+          :alt="block.alt"
+          :title="block.title"
+          loading="lazy"
+        />
       </div>
       <div v-if="block.type === 'cards'">
         <component-block-small :component="block" />
       </div>
       <div v-if="block.type === 'mainCompetencies'">
-        <main-competencies-component :component="block" :inComponent="inComponent" />
+        <main-competencies-component
+          :component="block"
+          :inComponent="inComponent"
+        />
       </div>
       <div v-if="block.type === 'cooperationForm'">
         <component-cooperation-form v-if="block.embedForm" />
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content-margin">
+  <div>
     <component-renderer
       v-for="(component, index) in landing.components"
       :key="index"
